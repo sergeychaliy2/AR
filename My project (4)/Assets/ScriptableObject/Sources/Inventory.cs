c02d494b755bf5371019968a033bc87e78474e7b
SergeyChaliy <70662544+sergeychaliy2@users.noreply.github.com>
2022-10-05 15:04:59 +0300

    .

--- My project (4)/Assets/ScriptableObject/Sources/Inventory.cs
+++ My project (4)/Assets/ScriptableObject/Sources/Inventory.cs
@@ -6,11 +6,18 @@ public class Inventory : MonoBehaviour
 {
     [SerializeField] List<Item> StartItems = new List<Item>();
     List<Item> inventoryItems = new List<Item>();
+    internal object InventoryItems;
+
     // Start is called before the first frame update
     void Start()
     {
         for (var i = 0; i < StartItems.Count; i++)
             AddItem(StartItems[i]);
+    }
+
+    void Update()
+    {
+
     }
 
     public void AddItem(Item item)
@@ -19,8 +26,5 @@ public class Inventory : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
 }
---
+++ My project (4)/Assets/ScriptableObject/Sources/UI/InventoryWindow.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryWindow : MonoBehaviour
+{
+    [SerializeField] Inventory targetInventory;
+    [SerializeField] RectTransform itemsPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Redraw()
+    {
+        for (var i=0; i<targetInventory.InventoryItems.Count; i++)
+        {
+            var item = targetInventory.InventoryItems[i];
+
+            var icon = new GameObject("Icon");
+            icon.AddComponent<Image>().sprite=item.Icon;
+        }
+    }
+}
